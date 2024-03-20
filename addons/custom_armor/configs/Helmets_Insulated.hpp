502ca1cba3f1ac21eb0c4217e218f47aa6bd874b
DartRuffian <skulldude327@gmail.com>
2024-03-19 22:43:03 -0500

    Fixed custom insulated and P1-2 helmets

--- addons/custom_armor/configs/Helmets_Insulated.hpp
+++ addons/custom_armor/configs/Helmets_Insulated.hpp
@@ -1,25 +1,34 @@
 class CLASS(Helmet_Phase2_Insulated_Base);
-class CLASS(Helmet_Phase2_Insulated_Dexus): CLASS(Helmet_Phase2_Insulated_Base)
-{
+class CLASS(Helmet_Phase2_Insulated_Dexus): CLASS(Helmet_Phase2_Insulated_Base) {
     displayName = "[KC] INF P2 Snow Helm ('Dexus')";
-    hiddenSelectionsTextures[] =
-    {
+    hiddenSelectionsTextures[] = {
         QPATHTOF(data\helmets\insulated\Dexus_camo1_co.paa)
     };
-};
-class CLASS(Helmet_Phase2_Insulated_Joe): CLASS(Helmet_Phase2_Insulated_Base)
-{
-    displayName = "[KC] INF P2 Snow Helm ('Joe')";
-    hiddenSelectionsTextures[] =
-    {
-        QPATHTOF(data\helmets\insulated\Joe_camo1_co.paa)
+
+    class XtdGearInfo {
+        model = QCLASS(Helmets_Insulated_Phase2_Custom);
+        custom = "Dexus";
     };
 };
-class CLASS(Helmet_Phase2_Insulated_Keeli): CLASS(Helmet_Phase2_Insulated_Base)
-{
+class CLASS(Helmet_Phase2_Insulated_Joe): CLASS(Helmet_Phase2_Insulated_Base) {
+    displayName = "[KC] INF P2 Snow Helm ('Joe')";
+    hiddenSelectionsTextures[] = {
+        QPATHTOF(data\helmets\insulated\Joe_camo1_co.paa)
+    };
+
+    class XtdGearInfo {
+        model = QCLASS(Helmets_Insulated_Phase2_Custom);
+        custom = "Joe";
+    };
+};
+class CLASS(Helmet_Phase2_Insulated_Keeli): CLASS(Helmet_Phase2_Insulated_Base) {
     displayName = "[KC] INF P2 Snow Helm ('Keeli')";
-    hiddenSelectionsTextures[] =
-    {
+    hiddenSelectionsTextures[] = {
         QPATHTOF(data\helmets\insulated\Keeli_camo1_co.paa)
+    };
+
+    class XtdGearInfo {
+        model = QCLASS(Helmets_Insulated_Phase2_Custom);
+        custom = "Keeli";
     };
 };
